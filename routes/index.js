e9103b4e0c8e4edd37d1340d86cba907acade3ae
claude009 <37622842+claude009@users.noreply.github.com>
2019-11-25 18:24:23 +0300

    File upload Database

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const book = require('../models/book')
+const book = require("../models/book");
 
-router.get("/", async(req, res) => {
-  let books
+router.get("/", async (req, res) => {
+  let books;
   try {
-    books = await book.find().sort({createdAt: 'desc'}).limit(10).exec()
+    books = await book
+      .find()
+      .sort({ createdAt: "desc" })
+      .limit(10)
+      .exec();
   } catch (error) {
-    books = []
+    books = [];
   }
-  res.render("index", {books: books});
+  res.render("index", { books: books });
 });
 
 module.exports = router;
